f763c177c73d3a23a9b9f9da973f9682c408a313
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2020-04-07 16:52:16 +0300

    Select Power up if there no power up selected added

--- Assets/Scripts/EntranceUI.cs
+++ Assets/Scripts/EntranceUI.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.Linq;
+
 public class EntranceUI : MonoBehaviour
 {
     [SerializeField]TextMeshProUGUI coinText,scoreText,totalScoreText;
@@ -28,6 +30,33 @@ public class EntranceUI : MonoBehaviour
     }
     public void PlaySurvival()
     {
+        MyPowerUpsAutoChooser();
         SceneManager.LoadScene(1);
+    }
+    void MyPowerUpsAutoChooser()
+    {
+        try
+        {
+            if(PowerUpManager.powerUpManager.selectedActivePowerUps.Count < 1)
+            {
+                foreach (PowerUpType item in PowerUpManager.powerUpManager.playerPowerUps)
+                {
+                    PowerUp p = PowerUpManager.powerUpManager.powerUps
+                        .FirstOrDefault(
+                            s => 
+                                s.powerUpType == item );
+                    if(p.usageType == UsageType.Temporary)
+                    {
+                        PowerUpManager.powerUpManager.SelectPowerUp(p);
+                        Debug.Log("a power up automaticly selected");
+                        break;
+                    }
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }        
     }
 }
